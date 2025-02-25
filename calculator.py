62472d4991aacf43ac197149e0e50a9c55546766
Bhavya <138712454+Jungkitty@users.noreply.github.com>
2025-02-25 23:16:17 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -14,4 +14,4 @@ match op:
     case "/":  
         print("Result:", "Error: Division by zero" if num2 == 0 else num1 / num2)
     case _:  
-         print("Invalid operator!")
+         print("Invalid operator.")
